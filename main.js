331cfb32830b0280e53b26d768cca6e439825732
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-04-13 21:12:15 +0530

    Modifying html file

---
+++ main.js
@@ -0,0 +1,41 @@
+const addButton = document.querySelector('.addButton');
+var inputValue = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+class item{
+    constructor(itemName){
+        this.createDiv(itemName);
+
+    }
+
+
+    createDive(itemName){
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input');
+        input.type = "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "EDIT";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "REMOVE";
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+
+
+    }
+}
+
+new item("Sport");
